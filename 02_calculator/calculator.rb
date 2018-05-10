06579e357c7190fe149d7a9b0bce01cd3e3b32f2
Diego Ortiz <diego.ortiz@youse.com.co>
2018-05-10 15:37:57 -0500

    Implementado tdd a partir de multiply, task calculator finished

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,37 @@
 #write your code here
+def add x, y
+    x + y 
+end
+
+def subtract x, y
+    x - y 
+end
+
+def sum array
+    sum = 0
+    if array.length
+        array.each{ |x| sum += x}
+    end
+    sum
+end
+
+def multiply (*arguments)
+    mul = 1
+    arguments.each { |x| mul*= x}
+    return mul
+end
+
+def power (x, y)
+    result = (x ** y).to_i
+    return result
+end
+
+def factorial factor
+    result = 1
+    x = 1
+    while x <= factor do
+        result *= x
+        x += 1
+    end
+    return result
+end
