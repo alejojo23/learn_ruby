ac9116268c35ec7f03439aca03f9556c2f34edfe
Diego Ortiz <diego.ortiz@youse.com.co>
2018-05-11 13:15:07 -0500

    Solved all but last test. Solution copied from StackOverflow, check later this part

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,36 @@
 #write your code here
+def echo (word)
+  "#{word}"
+end
+
+def shout (word)
+  word = word.upcase
+end
+
+def repeat (word, iterate = 2)
+  result = ''
+  iterate.times do |i|
+    (i > 0) ? result =  result + ' ' + word : result += word
+  end
+  result
+end
+
+def start_of_word (text, chars)
+  text[0..chars-1]
+end
+
+def first_word (text)
+  text.split(" ")[0]
+end
+
+
+def titleize (sentence)
+  small = %w{a an and the or for of over}
+  sentence.split.each_with_index.map{|word, index| small.include?(word) && index > 0 ? word : word.capitalize }.join(' ')
+
+  # FAILED TRY, BUT ALSO, TRY AGAIN LATER
+  # result = ''
+  # smallw = %w(and over the)
+  # sentence.index(' ') == nil ? result = sentence.capitalize : result = (sentence.split(' ').map(smallw.include ? sentence : &:capitalize).join(' '))
+  # result
+end
