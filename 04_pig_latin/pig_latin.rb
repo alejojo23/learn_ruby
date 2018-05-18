fef7851f3c0de85b681d813dc9ac3b64ca2f8863
Diego Ortiz <diego.ortiz@youse.com.co>
2018-05-18 10:34:51 -0500

    Test with new console colors

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,13 @@
 #write your code here
+def translate (word)
+  vowels = %w(a e i o u)
+  if word[0].split.map { |char| char.include?vowels.to_s }
+    word += 'ay'
+  else
+    word += word[0]
+  end
+  puts word
+  return word
+end
+
+translate("aramo")
