d67082ddd09ea8d8b3012504b9a52972e13aadd0
Diego Ortiz <diego.ortiz@youse.com.co>
2018-05-22 11:22:11 -0500

    Book titles solved

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,29 @@
 class Book
-# write your code here
+
+  attr_accessor :title
+
+  @concatenado = ''
+  
+
+  def title 
+    @nocapitalize = %w(a an the for and nor but or yet so in of)
+    @title.split(' ').each_with_index { |word, index| 
+      if index == 0
+        first = word[0].upcase
+        word[0] = first
+      else
+        if !@nocapitalize.include?(word)
+          first = word[0].upcase
+          word[0] = first
+        end
+      end
+      if index == 0
+        @concatenado = @concatenado.to_s + word
+      else 
+        @concatenado += ' ' + word  
+      end
+    }
+    return @concatenado
+  end
+
 end
